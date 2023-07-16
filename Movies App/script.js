e89a2c1eb4d607669df030b4317e65803bdcbaba
Manish Tiwari <manisht.mt.mt@gmail.com>
2023-07-16 12:16:00 +0530

    movie app

---
+++ Movies App/script.js
@@ -0,0 +1,67 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3065345e99c53baae4e6d192676cd677";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=3065345e99c53baae4e6d192676cd677&query=";
+
+const main = document.querySelector("#main");
+const form = document.querySelector("#form");
+const search = document.querySelector("#search");
+
+async function getMovies(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  showMovies(data.results);
+}
+
+getMovies(API_URL);
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}" />
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRating(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRating(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
